9f6eedc2ba1ba212751352c272454f5e3d94db5a
Keith Jasper Quevada <keithjasper.quevada@gmail.com>
2026-07-28 11:59:39 +0800

    Repository

---
+++ src/test/java/org/rocs/osdrmsa/OsdrmsaApplApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rocs.osdrmsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsdrmsaApplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
