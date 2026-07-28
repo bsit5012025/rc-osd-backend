0388daee4e246a7f3a73dcdb9d5e7a9e8925a920
keithjasperquevada <keithjasper.quevada@gmail.com>
2026-07-28 12:04:12 +0800

    Repository (#1)

---
+++ src/main/java/org/rocs/osdrmsa/OsdrmsaApplApplication.java
@@ -0,0 +1,13 @@
+package org.rocs.osdrmsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsdrmsaApplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsdrmsaApplApplication.class, args);
+	}
+
+}
